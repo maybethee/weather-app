1854b67d04fdbbc8dbdf5f63fec4ced2b7760a2e
retra <jozalamun@gmail.com>
2024-07-13 10:23:50 -0400

    Fetch current weather using Visual Crossing API

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 import "./style.css";
 
-console.log("connected");
+const city = "london";
+
+async function fetchCurrentWeather(city) {
+  try {
+    const response = await fetch(
+      `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?unitGroup=us&include=current&key=2WV3PSAMUFM8QTJ9BUZKB28CA&contentType=json`,
+      {
+        mode: "cors",
+        method: "GET",
+        headers: {},
+      }
+    );
+    if (!response.ok) {
+      throw new Error("HTTP error " + response.status);
+    }
+    const json = await response.json();
+    console.log(json);
+    console.log(
+      `temp: ${json.currentConditions.temp}° Fahrenheit\n
+      conditions: ${json.currentConditions.conditions}\n
+      icon: `
+    );
+  } catch (error) {
+    console.log("Error: " + error.message);
+  }
+}
+
+fetchCurrentWeather(city);
