be52fee9c37a7a84da48707ea6a47adcb8f90170
retra <jozalamun@gmail.com>
2024-07-14 09:36:23 -0400

    Add button for unit conversion

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import "./style.css";
 
+const tempDisplay = document.querySelector(".temp");
+const conditionsDisplay = document.querySelector(".conditions");
+const iconDisplay = document.querySelector(".icon");
+const convertBtn = document.querySelector(".unitGroup");
+convertBtn.setAttribute("style", "visibility: hidden");
 let temp = null;
-let conditions = "";
-let icon = "";
 
 async function fetchCurrentWeather(city) {
   try {
@@ -19,20 +22,48 @@ async function fetchCurrentWeather(city) {
     }
     const json = await response.json();
     console.log(json);
-    console.log(
-      `temp: ${json.currentConditions.temp}° Fahrenheit\n
-      conditions: ${json.currentConditions.conditions}\n
-      icon: `
-    );
+    temp = Math.round(json.currentConditions.temp);
+    const conditions = json.currentConditions.conditions;
+    const icon = json.currentConditions.icon;
+
+    return {
+      temp,
+      conditions,
+      icon,
+    };
   } catch (error) {
     console.log("Error: " + error.message);
+    return {
+      temp: null,
+      conditions: "Error fetching data",
+      icon: "",
+    };
   }
 }
 
-const searchBtn = document.querySelector("button");
+const form = document.querySelector("form");
 
-searchBtn.addEventListener("click", (event) => {
+form.addEventListener("submit", async (event) => {
   event.preventDefault();
+
   const city = document.querySelector("input").value;
-  fetchCurrentWeather(city);
+  const { temp, conditions, icon } = await fetchCurrentWeather(city);
+
+  tempDisplay.textContent = `${temp}°F`;
+  conditionsDisplay.textContent = conditions;
+  iconDisplay.textContent = icon;
+  convertBtn.setAttribute("style", "visibility: visible");
+  convertBtn.textContent = "°C";
+});
+
+convertBtn.addEventListener("click", () => {
+  if (convertBtn.textContent === "°C") {
+    temp = Math.round(((temp - 32) * 5) / 9);
+    tempDisplay.textContent = `${temp}°C`;
+    convertBtn.textContent = "°F";
+  } else {
+    temp = Math.round((temp * 9) / 5 + 32);
+    tempDisplay.textContent = `${temp}°F`;
+    convertBtn.textContent = "°C";
+  }
 });
